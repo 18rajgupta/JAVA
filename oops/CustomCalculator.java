bd87c0b1393b2b5cb6d6177ebac44471eefde374
Raj Gupta <122553098+18rajgupta@users.noreply.github.com>
2025-10-05 03:51:09 +0530

    init commit

--- oops/CustomCalculator.java
+++ oops/CustomCalculator.java
@@ -1,9 +1,82 @@
 package oops;
 
-import java.util.Scanner;
+class InvalidInputException extends Exception {
+    @Override
+    public String toString() {
+        return "cannot input 8 or 9";
+    }
+}
+
+class CannotDivideByZeroException extends Exception {
+    @Override
+    public String toString() {
+        return "cannot divide by 0";
+    }
+}
+
+class MaxInputException extends Exception {
+    @Override
+    public String toString() {
+        return "Input can't be greater than 100000";
+    }
+}
+
+class MaxMultiplyInputException extends Exception {
+    @Override
+    public String toString() {
+        return "Input can't be greater than 7000 while multiplying";
+    }
+}
+
+class CustomCalc {
+    double add(double a, double b) throws InvalidInputException, MaxInputException {
+        if (a == 8 || a == 9 || b == 8 || b == 9) {
+            throw new InvalidInputException();
+        }
+        if (a > 100000 || b > 100000) {
+            throw new MaxInputException();
+        }
+        return a + b;
+    }
+
+    double subtract(double a, double b) throws InvalidInputException, MaxInputException {
+        if (a == 8 || a == 9 || b == 8 || b == 9) {
+            throw new InvalidInputException();
+        }
+        if (a > 100000 || b > 100000) {
+            throw new MaxInputException();
+        }
+        return a - b;
+    }
+
+    double multiply(double a, double b) throws InvalidInputException, MaxInputException, MaxMultiplyInputException {
+        if (a == 8 || a == 9 || b == 8 || b == 9) {
+            throw new InvalidInputException();
+        }
+        if (a > 100000 || b > 100000) {
+            throw new MaxInputException();
+        } else if (a > 7000 || b > 7000) {
+            throw new MaxMultiplyInputException();
+        }
+        return a * b;
+    }
+
+    double divide(double a, double b) throws CannotDivideByZeroException, InvalidInputException, MaxInputException {
+        if (a == 8 || a == 9 || b == 8 || b == 9) {
+            throw new InvalidInputException();
+        }
+        if (a > 100000 || b > 100000) {
+            throw new MaxInputException();
+        }
+        if (b == 0) {
+            throw new CannotDivideByZeroException();
+        }
+        return a / b;
+    }
+}
 
 public class CustomCalculator {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InvalidInputException, CannotDivideByZeroException, MaxInputException, MaxMultiplyInputException {
         /*
         Exercise: You have to create a custom calculator with following operations:
         1. + -> Addition
@@ -17,14 +90,10 @@ public class CustomCalculator {
         4. Max Multiplier reached Exception - Don't allow any multiplication input to be greater than 7000
          */
 
-
-        Scanner sc = new Scanner(System.in);
-        try {
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-        } catch (Exception e) {
-            System.out.println("Invalid Input Exception !!");
-        }
-
+        CustomCalc c = new CustomCalc();
+//        System.out.println(c.add(7,8));
+//        System.out.println(c.divide(7,0));
+//        System.out.println(c.add(2, 1000000));
+        System.out.println(c.multiply(3, 8300));
     }
 }
